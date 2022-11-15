2bdc087810192533df2a02574c05a8138f61262f
Pierre Chris Wesley <pierrechriswesley@gmail.com>
2022-11-15 17:27:30 -0500

    almost done

--- 2/7.js
+++ 2/7.js
@@ -1,6 +1,8 @@
 let vowel = ['a','e','i','o','u','y']
 let phrase = "Ayibobo Ayiti"
 
-
-
+for(let i=0; i<phrase.length; i++){
+    if(vowel.includes(phrase[i]))
+        phrase = phrase.replace(phrase[i-1],"*")
+}
 console.log(phrase);
